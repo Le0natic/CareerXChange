4413170da8744d3b59f074aeefca10bc8fb1b374
NCS\P1333378 <romes8001@gmail.com>
2026-04-06 21:36:45 +0800

    fix to add to log file

--- python/shared/logger.py
+++ python/shared/logger.py
@@ -28,10 +28,6 @@ def setup_logger(name: str) -> logging.Logger:
     )
     file_handler.setFormatter(formatter)
 
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-
     logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
 
     return logger
