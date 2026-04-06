edf87b0ea177d7ebb33b6a3986596f0244991b68
NCS\P1333378 <romes8001@gmail.com>
2026-04-06 20:42:02 +0800

    fix test

--- python/shared/llm_client.py
+++ python/shared/llm_client.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
 llm_name = "gpt-4.1-mini"
 
 def call_llm(system_prompt: str, user_input: str, response_type: str) -> str:
+    client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
     response = client.chat.completions.create(
         model=llm_name,
         messages=[
